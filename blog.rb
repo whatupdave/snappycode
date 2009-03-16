e5c4b3dd903cd4985291a75a3bbaf8d9bc1a0734
Dave Newman <david.newman@sentia.com.au>
2009-03-16 13:43:40 -0500

    Random styling and started a new post

--- blog.rb
+++ blog.rb
@@ -1,5 +1,6 @@
 require 'rubygems'
 require 'sinatra'
+require 'bluecloth'
 
 posts_dir = File.dirname(__FILE__) + '/posts/'
 
@@ -8,15 +9,23 @@ class Post
   attr_reader :date
   attr_reader :title
   attr_reader :content
+  attr_reader :format
   
   def initialize(filename)
-    /(\d{4}-\d{2}-\d{2})-([^\.]*)/.match filename
+    /(\d{4}-\d{2}-\d{2})-([^\.]*)\.(.*)/.match filename
     date_string = $1
     title_string = $2
+    @format = $3
     @date = Date.parse(date_string)
     @title = title_string.gsub('-',' ')
     @url = "/posts/#{date_string}-#{title_string}"
     @content = File.open(filename).read
+  end
+end
+
+helpers do
+  def write_post_content(post)
+    BlueCloth.new(post.content).to_html
   end
 end
 
@@ -27,7 +36,7 @@ end
 
 get '/' do
   @posts = []
-  Dir.glob(posts_dir + '*.haml') do |f|
+  Dir.glob(posts_dir + '*.*') do |f|
     @posts << Post.new(f)
   end
   
@@ -35,6 +44,6 @@ get '/' do
 end
 
 get '/posts/:post' do
-  @post = Post.new(posts_dir + params[:post] + '.haml')
+  @post = Post.new(posts_dir + params[:post] + '.markdown')
   haml :show
 end
